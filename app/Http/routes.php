d5884d9bbfe8e162f454feb2b5f4d441419ce4ea
Jesús <jesus@shivaweb.es>
2016-07-15 13:22:54 +0200

    primer commit

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,15 +1,1 @@
 <?php
-
-Route::get('/header',function(){
-  return response("Hello", 200)->header('Content-Type', 'text/html');
-});
-
-Route::get('/cookie',function(){
-   return response("Hello", 200)->header('Content-Type', 'text/html')
-      ->withcookie('name','Virat Gandhi');
-});
-
-
-Route::get('insert', 'StudInsertController@insertform');
-Route::post('create', 'StudInsertController@insert');
-Route::get('view', 'StudViewController@index');
--- app/Student.php
+++
@@ -1,9 +0,0 @@
-<?php
-// \App\Student
-namespace App;
-use Illuminate\Database\Eloquent\Model;
-
-class Student extends Model
-{
-  protected $table = 'students';
-}
