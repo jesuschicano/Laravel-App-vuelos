27698a32354a2fe59a096ce3cc1f83ca4f1c178c
Jesús <jesus@shivaweb.es>
2016-07-20 12:42:25 +0200

    perrea

--- resources/views/vuelos/index.blade.php
+++ resources/views/vuelos/index.blade.php
@@ -9,13 +9,15 @@
       <th>NOMBRE</th>
       <th>AEROLINEA</th>
       <th></th>
+      <th></th>
     </thead>
   @foreach ($vuelos as $vuelo)
     <tr>
       <td>{{ $vuelo->id }}</td>
       <td>{{ $vuelo->nombre }}</td>
       <td>{{ $vuelo->aerolinea }}</td>
-      <td>Editar</td>
+      <td><a href="#" class="btn btn-warning"><span class="glyphicon glyphicon-edit"></span></a></td>
+      <td><a href="#" class="btn btn-danger"><span class="glyphicon glyphicon-trash"></span></a></td>
     </tr>
   @endforeach
   </table>
